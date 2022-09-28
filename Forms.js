af5edad1165782e8ffc16a8b1215d60c62a98807
Andres Sanchez <andresana86@gmail.com>
2022-09-28 09:51:11 -0500

    withstyles

---
+++ Forms.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+//const colores = {
+    //borde: "#0075FF",
+    //error: "bb2929",
+    //succs: "1ed12d"
+///
+
+//const Formulario = () => styled.form 
+//display = grid
+//};
+
+
+
+
+
+export {Formulario}
