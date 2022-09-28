b791275014d9d91413caeb7271131eada9b7316d
Andres Sanchez <andresana86@gmail.com>
2022-09-28 09:46:54 -0500

    jsfiles

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+const App = () => {
+  return ( 
+    <main>
+      <form action="">
+        <label htmlFor="">Usuario</label>
+        <input type="text" placeholder="usuario" />
+        <p>Andres</p>
+      </form>
+    </main>
+   );
+}
+ 
+
+
+export default App;
